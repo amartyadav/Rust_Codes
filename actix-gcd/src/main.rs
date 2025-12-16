1cf904c9464a73b6fd85d74827057c51a6cccf8b
Amartya Yadav <amartyadav@gmail.com>
2025-12-17 00:50:10 +0530

    Add projects as regular directories

---
+++ actix-gcd/src/main.rs
@@ -0,0 +1,66 @@
+use std::fmt::format;
+
+use actix_web::{web, App, HttpResponse, HttpServer, Responder};
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+struct GcdParameters {
+    n: u64,
+    m: u64,
+}
+
+#[actix_web::main]
+async fn main() {
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
+    });
+
+    println!("Server starting on http://localhost:3000...");
+    server
+        .bind("127.0.0.1:3000").expect("Error binding server to address")
+        .run().await.expect("Error running server");
+}
+
+async fn get_index() -> impl Responder {
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(
+            r#"
+            <title>GCD Calculator</title>
+            <form action="/gcd" method="post">
+                <input type="text" name="n" />
+                <input type="text" name="m" />
+                <button type="submit">Compute GCD</button>
+            </form>
+            "#,
+        )
+}   
+
+async fn post_gcd(form: web::Form<GcdParameters>) -> impl Responder {
+    if form.n == 0 || form.m == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Don't use 0s to compute GCD");
+    }
+
+    let response = format!("The greatest common divisor of the numbers {} and {} is <b>{}<b>\n", form.m, form.n, gcd(form.m, form.n));
+
+    HttpResponse::Ok()
+        .content_type("text/html")
+        .body(response)
+}
+
+fn gcd(mut m: u64, mut n: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        if m < n {
+            let t = m;
+            m = n;
+            n = t;
+        }
+        m = m % n;
+    }
+    n
+}
